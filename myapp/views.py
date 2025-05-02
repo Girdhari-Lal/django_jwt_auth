0ff99943555d7788ee3d79b31411203f5cd10ac8
Girdharilal12 <girdharilal2000@outlook.com>
2025-05-02 22:03:51 +0500

    in views files remove APIView add generics.CreateAPIView

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status
@@ -7,18 +8,11 @@ from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-class RegisterView(APIView):
+class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
 
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({
-                'message': 'User registered successfully',
-                'user': UserSerializer(user).data
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
